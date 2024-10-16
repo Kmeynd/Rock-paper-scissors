914e810f45ada02ce56a8e41c7b19e034368c157
kevin <kmeynard.1@gmail.com>
2024-10-16 20:31:05 +0900

    added a loop

--- testjavascript.js
+++ testjavascript.js
@@ -16,37 +16,36 @@ function getHumanChoice(){
     return x
 }
 
-function scores(winner){
-    humanScore=0
-    computerScore=0
-
-   if (winner=="human"){
-    humanScore+=1
-   }else if(winner=="computer"){
-    computerScore+=1
-   }else{
-    return `it's a draw !`
-   }
-
-
-   return `the score is ${humanScore} for you and ${computerScore} for the computer`
-}
-
-
 function playRound(humanChoice,computerChoice){
     
     if (humanChoice=="scissors"&&computerChoice=="paper"||humanChoice=="paper"&&computerChoice=="rock"||humanChoice=="rock"&&computerChoice=="scissors"){
-        console.log(scores('human'))
+        return 'human'
     }else if(humanChoice==computerChoice){
-        console.log(scores('draw'))
+        return 'draw'
     }else{
-        console.log(scores('computer'))
-    }
-
-    
+       return 'computer'
+    } 
 }
 
-const humanSelection = getHumanChoice()
-const computerSelection= getComputerChoice()
 
-playRound(humanSelection,computerSelection)
+
+function playGame(){
+    humanScore=0
+    computerScore=0
+
+    for (let i=0;i<5;i++){
+        const humanSelection = getHumanChoice()
+        const computerSelection= getComputerChoice()
+        winner=playRound(humanSelection,computerSelection)
+
+       if (winner=="human"){
+        humanScore+=1
+       }else if(winner=="computer"){
+        computerScore+=1
+       }else{
+        console.log(`it's a draw !`)
+       }
+       console.log(`the score is ${humanScore} for you and ${computerScore} for the computer`) 
+} }
+
+console.log(playGame())
