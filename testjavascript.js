c3d2919ce05ea32e2c05510cf40454d5531260b5
kevin <kmeynard.1@gmail.com>
2024-10-18 21:48:44 +0900

    Added ui

--- testjavascript.js
+++ testjavascript.js
@@ -8,12 +8,7 @@ function getComputerChoice(){
     }else{
         return "paper"
     }
-    
-}
 
-function getHumanChoice(){
-    let x= prompt("Please enter a number (rock, paper, scissors) : ")
-    return x
 }
 
 function playRound(humanChoice,computerChoice){
@@ -29,23 +24,70 @@ function playRound(humanChoice,computerChoice){
 
 
 
-function playGame(){
-    humanScore=0
-    computerScore=0
+function playGame(choice,humanScore,computerScore){
+        
+    const humanSelection = choice
+    const computerSelection= getComputerChoice()
+    winner=playRound(humanSelection,computerSelection)
 
-    for (let i=0;i<5;i++){
-        const humanSelection = getHumanChoice()
-        const computerSelection= getComputerChoice()
-        winner=playRound(humanSelection,computerSelection)
+    if (winner=="human"){
+    humanScore+=1
+    }else if(winner=="computer"){
+    computerScore+=1
+    }else{
+    return `it's a draw !`
+    }
+    return `the score is ${humanScore} for you and ${computerScore} for the computer` 
+}
 
-       if (winner=="human"){
-        humanScore+=1
-       }else if(winner=="computer"){
-        computerScore+=1
-       }else{
-        console.log(`it's a draw !`)
-       }
-       console.log(`the score is ${humanScore} for you and ${computerScore} for the computer`) 
-} }
 
-console.log(playGame())
+
+const btns = document.querySelectorAll('button')
+
+const div1= document.querySelector('div')
+let div = document.createElement('div')
+div.textContent="Start to play!"
+div1.appendChild(div)
+humanScore=0
+computerScore=0
+clicked=0
+
+btns.forEach(function(item){
+    item.addEventListener("click",()=>{
+        if (humanScore<5 && computerScore<5){
+        str=playGame(item.value,humanScore,computerScore)
+        div.textContent=str
+            if (str!="it's a draw !"){  
+            humanScore=Number(str[13])
+            computerScore=Number(str[27])
+            }else{
+                humanScore=humanScore
+                computerScore=computerScore
+            }
+        div1.appendChild(div)
+        }if((humanScore==5 || computerScore==5)&& clicked==0 ){
+            clicked=1
+            div.textContent=`Final score ${humanScore} | ${computerScore}`
+            div1.appendChild(div)
+            let p=document.createElement("p")
+            if (humanScore>computerScore){
+                p.textContent="You Won!"
+            }else{
+                p.textContent="You Lose!"
+            }
+            div1.appendChild(p)
+            let but=document.createElement("button")
+            but.textContent="restart"
+            div1.append(but)
+            but.addEventListener("click",()=>{
+                clicked=0
+                humanScore=0
+                computerScore=0
+                div1.removeChild(but)
+                div1.removeChild(p)
+                div.textContent="Start to play!"
+
+            })
+        }
+    })
+})
